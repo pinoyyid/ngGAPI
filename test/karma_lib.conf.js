6520db788e7c73764ecdcd1283186734669af2fc
Roy Smith <roy.smith.esq@gmail.com>
2015-04-05 06:15:45 +0100

    move bower down into nggapi_lib directory and updte for A 1.4

--- test/karma_lib.conf.js
+++ test/karma_lib.conf.js
@@ -25,9 +25,9 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/angular-sanitize/angular-sanitize.js',
+      'nggapi_lib/bower_components/angular/angular.js',
+      'nggapi_lib/bower_components/angular-mocks/angular-mocks.js',
+      'nggapi_lib/bower_components/angular-sanitize/angular-sanitize.js',
       'nggapi_lib/libscripts/services/oauth_s.js',
       'nggapi_lib/appscripts/**/*.js',
       'nggapi_lib/libscripts/**/*.js',
